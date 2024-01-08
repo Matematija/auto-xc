3a595466e798cc966aa3b39f49953e6c5883dc64
Matija Medvidovic <matija.medvidovic@gmail.com>
2024-01-07 23:29:26 -0500

    Repo init hygiene

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+BASE_DEPENDENCIES = ["numpy", "jax", "pyscf"]
+
+setup(
+    name="autoxc",
+    author="Matija Medvidovic",
+    url="https://github.com/Matematija/auto-xc",
+    author_email="matija.medvidovic@gmail.com",
+    description="Automatic differentiation tools for interfacing custom density functionals with libXC in quantum chemistry.",
+    packages=find_packages(),
+    install_requires=BASE_DEPENDENCIES,
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Unix",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Chemistry",
+    ],
+)
